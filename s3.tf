0a94c2bae0649d1c13bfcee1a87106e80ee3243a
Alex Kunev <akkunev@gmail.com>
2026-01-28 20:31:42 +0200

    Ummmm added module... duh

--- s3.tf
+++ s3.tf
@@ -2,6 +2,9 @@ resource "random_id" "this" {
   byte_length = 4
 }
 
-resource "aws_s3_bucket" "this" {
-  bucket = "terraform-course-cloud-${random_id.this.hex}"
+module "s3" {
+  source      = "app.terraform.io/alexk-learn/s3/aws"
+  version     = "0.0.1"
+  vpc_cidr    = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
 }
